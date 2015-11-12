ee88622772fa7590e3ff09838062763a53825aba
DongHee Kim <opklnm102@gmail.com>
2015-11-12 12:40:18 +0900

    add homework1

---
+++ mydiary/src/main/java/me/dong/mydiary/MainActivity.java
@@ -0,0 +1,78 @@
+package me.dong.mydiary;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvDate;
+    Button btnSave;
+    EditText etContents;
+
+    Calendar cal;
+    int cYear;
+    int cMonth;
+    int cDay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("My Diary");
+
+        cal = Calendar.getInstance();
+        cYear = cal.get(Calendar.YEAR);
+        cMonth = cal.get(Calendar.MONTH) + 1;
+        cDay = cal.get(Calendar.DAY_OF_MONTH);
+
+        tvDate = (TextView) findViewById(R.id.textView_date);
+        btnSave = (Button) findViewById(R.id.button_save);
+        etContents = (EditText) findViewById(R.id.editText_contents);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
